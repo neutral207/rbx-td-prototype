4eb9ff1b8b7ce458454ed7d015aab00cf8aeea9e
neutral207 <162142226+neutral207@users.noreply.github.com>
2026-01-14 21:57:40 -0500

    Updated Money System logic on Tower.lua

--- ServerScripts/Tower.lua
+++ ServerScripts/Tower.lua
@@ -1,12 +1,16 @@
 local serverStorage = game:GetService("ServerStorage")
 local phys = game:GetService("PhysicsService")
 local RepStorage = game:GetService("ReplicatedStorage")
-local events =  RepStorage:WaitForChild("Events")
-local spawnTowerEvent = events:WaitForChild("SpawnTower")
-local tower = {}
 
-local function findNearestEnemy(newTower)
-	local maxDistance = 75
+local events =  RepStorage:WaitForChild("Events")
+local functions =  RepStorage:WaitForChild("Functions")
+local requestTower = functions:WaitForChild("RequestTower")
+local spawnTowerEvent = events:WaitForChild("SpawnTower")
+
+local tower = {}
+local maxTowers = 15
+
+local function findNearestEnemy(newTower, range)
 	local nearestTarget = nil
 
 	for i, target in ipairs(workspace.MapEnemies:GetChildren()) do
@@ -14,33 +18,39 @@ local function findNearestEnemy(newTower)
 		local distance = (targetPos.Position - newTower.HumanoidRootPart.Position).Magnitude
 		--print (target.Name, distance)
 
-		if distance < maxDistance then
+		if distance < range then
 			--print(target.Name, "is the nearest enemy")
 			nearestTarget = target
-			maxDistance = distance
+			range = distance
 		end
 	end
 	return nearestTarget
 end
 
-function tower.Attack(newTower)
-	local target = findNearestEnemy(newTower)
+function tower.Attack(newTower, player)
+	local config = newTower.Configuration
+	local target = findNearestEnemy(newTower, config.AttackRange.Value)
 	if target and target:FindFirstChild("Humanoid") and target.Humanoid.Health > 0 then
 		local targetCFrame = CFrame.lookAt(newTower.HumanoidRootPart.Position, target.HumanoidRootPart.Position)
 		newTower.HumanoidRootPart.BodyGyro.CFrame = targetCFrame
-		target.Humanoid:TakeDamage(25)
+		target.Humanoid:TakeDamage(config.AttackDamage.Value)
+		
+		if target.Humanoid.Health <= 0 then
+			player.leaderstats.Cash.Value += target.Humanoid.MaxHealth / 2
+		end
+		task.wait(config.FireRate.Value)
 	end
-
-	task.wait(1)
-	tower.Attack(newTower)
+	
+	task.wait(0.1)
+	tower.Attack(newTower, player)
 end
 
 
 function tower.Spawn(player, name, cframe)
-	local towerExists = RepStorage.Towers:FindFirstChild(name)
+	local allowedToSpawn = tower.CheckSpawn(player, name)
 	
-	if towerExists then
-		local newTower = towerExists:Clone()
+	if allowedToSpawn then
+		local newTower = RepStorage.Towers[name]:Clone()
 		newTower.Parent = workspace.Towers
 		newTower.HumanoidRootPart.CFrame = cframe
 		newTower.HumanoidRootPart:SetNetworkOwner(nil)
@@ -56,8 +66,11 @@ function tower.Spawn(player, name, cframe)
 				object.CollisionGroup = "Towers"
 			end
 		end
+		
+		player.leaderstats.Cash.Value -= newTower.Configuration.Price.Value
+		player.PlacedTowers.Value += 1
 			
-		coroutine.wrap(tower.Attack)(newTower)
+		coroutine.wrap(tower.Attack)(newTower, player)
 		
 	else
 		warn("Requested Tower does not exist")
@@ -65,4 +78,26 @@ function tower.Spawn(player, name, cframe)
 end
 
 spawnTowerEvent.OnServerEvent:Connect(tower.Spawn)
+
+function tower.CheckSpawn(player, name)
+	local towerExists = RepStorage.Towers:FindFirstChild(name)
+
+	if towerExists then
+		if towerExists.Configuration.Price.Value <= player.leaderstats.Cash.Value then
+			if player.PlacedTowers.Value < maxTowers then
+				return true
+			else
+				warn("Cannot place more than " .. maxTowers .. " towers!")
+			end
+		else
+			warn("Not enough cash to place this tower!")
+		end
+	else
+		warn("Requested Tower does not exist")
+	end
+	
+end
+
+requestTower.OnServerInvoke = tower.CheckSpawn
+
 return tower
